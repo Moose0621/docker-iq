23be33b5bef4246d5046e502534cbc66a481bad0
hosenblad <amccoy@sonatype.com>
2018-12-13 16:36:09 -0500

    Right file now, 1 line deleted

---
+++ scripts/cli.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+exec > /etc/webhook/output.log 2>&1
+
+echo $1
+echo $2
+echo $3
+cd /etc/webhooks;
+docker pull $2:my-webhook-test
+docker save -o docker.tar $2:my-webhook-test
+java -jar nexus-iq-cli-1.55.0-01.jar -s http://localhost:8070 -i juice-shop -a admin:admin123 docker.tar
+ls -la
